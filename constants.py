1cca2218d88472adda94551f64ad4a68c34ff008
GELIOS-19 <64653918+GELIOS-19@users.noreply.github.com>
2022-10-30 17:27:49 -0400

    Update constants.py

--- constants.py
+++ constants.py
@@ -2,7 +2,7 @@ DOMAIN = "http://127.0.0.1:5000"
 
 # noinspection SpellCheckingInspection
 GOOGLE_CLIENT_ID = (
-    "1026035935263-1o8l4e63v62j31jtteeu6clqne7r7rdu.apps.googleusercontent.com"
+    "*SECRET*"
 )
 
 GOOGLE_API_SCOPES = [
@@ -10,4 +10,4 @@ GOOGLE_API_SCOPES = [
     "https://www.googleapis.com/auth/userinfo.profile",
 ]
 
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY = "*SECRET*"
